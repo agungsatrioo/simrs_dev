5213a7a9eee12438bc88b767af27d0d32f653d15
agungsatrioo <guungofficial@gmail.com>
2020-09-14 20:44:03 +0700

    fix some bug

--- application/views/template.php
+++ application/views/template.php
@@ -54,7 +54,7 @@
 
             <header class="main-header">
                 <!-- Logo -->
-                <a href="<?php echo base_url() ?>adminlte/index2.html" class="logo">
+                <a href="<?=base_url()?>" class="logo">
                     <!-- mini logo for sidebar mini 50x50 pixels -->
                     <span class="logo-mini"><b>A</b>LT</span>
                     <!-- logo for regular state and mobile devices -->
