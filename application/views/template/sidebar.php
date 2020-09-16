bf4b80fc04f3e9260f15dc5cd32a3d8276198f80
agungsatrioo <guungofficial@gmail.com>
2020-09-16 20:09:35 +0700

    add active class on sidebar

--- application/views/template/sidebar.php
+++ application/views/template/sidebar.php
@@ -13,7 +13,6 @@
     <!-- sidebar menu: : style can be found in sidebar.less -->
     <ul class="sidebar-menu" data-widget="tree">
        
-        
         <?php
         // display all data main menu
         $this->db->where('is_main_menu',0);
@@ -25,24 +24,45 @@
             $this->db->where('is_main_menu',$menu->id_menu);
             $this->db->where('is_aktif','y');
             $submenu = $this->db->get('tbl_menu');
+
             if($submenu->num_rows()>0){
+                $submenu_list = "";
+                $tree_expand = false;
+                $menu_open_str = "";
+                $display_style_str = "display: none";
+                $pull_span_str = "<span class='pull-right-container'><i class='fa fa-angle-left pull-right'></i></span>";
+
+                foreach ($submenu->result() as $sub){
+                    $is_submenu_current = current_url() == base_url($sub->url);
+
+                    $sub_active = $is_submenu_current ? "class='active'" : "";
+
+                    if($is_submenu_current && !$tree_expand) {
+                        $tree_expand = true;
+                        $menu_open_str = "menu-open";
+                        $display_style_str = "display:block";
+                        $pull_span_str = "";
+                    }
+
+                     $submenu_list .= "<li $sub_active>".anchor($sub->url,"<i class='$sub->icon'></i> ".strtoupper($sub->title))."</li>"; 
+                }
+
                 // display sub menu
-                echo "<li class='treeview'>
+                echo "<li class='treeview $menu_open_str'>
                         <a href='#'>
                             <i class='$menu->icon'></i> <span>".strtoupper($menu->title)."</span>
-                            <span class='pull-right-container'>
-                                <i class='fa fa-angle-left pull-right'></i>
-                            </span>
+                            $pull_span_str
                         </a>
-                        <ul class='treeview-menu' style='display: none;'>";
-                        foreach ($submenu->result() as $sub){
-                            echo "<li>".anchor($sub->url,"<i class='$sub->icon'></i> ".strtoupper($sub->title))."</li>"; 
-                        }
-                        echo" </ul>
-                    </li>";
+                        <ul class='treeview-menu' style='$display_style_str;'>
+                        $submenu_list
+                        </ul>
+                        </li>
+                        ";
             }else{
                 // display main menu
-                echo "<li>";
+                $menu_active = current_url() == base_url($menu->url) ? "class='active'" : "";
+
+                echo "<li $menu_active>";
                 echo anchor($menu->url,"<i class='".$menu->icon."'></i> ".strtoupper($menu->title));
                 echo "</li>";
             }
